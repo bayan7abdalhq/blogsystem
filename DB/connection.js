09685395bef6ff4119bdd4ed2c2787480bf237ed
bayan7abdalhq <s12114374@stu.najah.edu>
2024-02-24 23:15:35 +0200

    change database

--- DB/connection.js
+++ DB/connection.js
@@ -1,6 +1,7 @@
 import { Sequelize } from 'sequelize';
-export const sequelize = new Sequelize('blogsystem', 'root', '', {
-    host: 'localhost',
+export const sequelize = new Sequelize('freedb_blogsystem3', 'freedb_bayan', 'wUCeQ%Re6UVCEw9', {
+    host: 'sql.freedb.tech',
+    port:3306,
     dialect: 'mysql'
   });
 
